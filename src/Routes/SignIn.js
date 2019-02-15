efdb420f126be9497cf5521ef2b70a691c4c7fe2
github username <matthewdizon94@protonmail.com>
2019-02-15 10:03:47 -0500

    redux kind of implemented

--- src/Routes/SignIn.js
+++ src/Routes/SignIn.js
@@ -1,12 +1,29 @@
 import React from 'react'
+import { signIn } from "../Actions/UserActions";
+import { connect } from "react-redux";
+import { withRouter } from "react-router-dom";
 
 class SignIn extends React.Component {
+    state = {
+        username : "",
+        password: ""
+    }
+    changeHandler = e => {
+      this.setState({
+        [e.target.name]: e.target.value
+      });
+    };
+    submitHandler = e => {
+        e.preventDefault()
+        this.props.signIn(this.state)
+    }
   render () {
+      console.log(this.props)
     return (
         <div className = "SignUp">
         <div className = "form-container">
             <div className="login-form">
-            <form>
+            <form onChange = {this.changeHandler} onSubmit = {this.submitHandler}>
                 <h2 className="text-center">Log in</h2>
                     <div className="form-group">
                         <input type="text" className="form-control" name = "username"
@@ -31,5 +48,17 @@ class SignIn extends React.Component {
     )
   }
 }
+const mapDispatchToProps = dispatch => {
+  return {
+    signIn: (user) => {
+      dispatch(signIn(user));
+    }
+  };
+};
 
-export default SignIn;
+export default withRouter(
+  connect(
+    null,
+    mapDispatchToProps
+)(SignIn)
+);
